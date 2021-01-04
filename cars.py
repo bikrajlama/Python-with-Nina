960401c27106cbd412e58a08b74dad257146e02d
bikrajlama <bxl038@shsu.edu>
2021-01-04 09:47:54 -0600

    ini

--- cars.py
+++ cars.py
@@ -1,8 +1,14 @@
 class Car:
     runs = True
 
-    def start(self, name):
+
+
+    def __init__(self, name):
+        print("New Car!")
         self.name = name
+
+
+    def start(self):
         if self.runs:
             print(f"{self.name} Car is started.")
         else:
