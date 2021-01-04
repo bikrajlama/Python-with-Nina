3f8e9dc78cf6df16930615427d2a4b0393552b8c
bikrajlama <bxl038@shsu.edu>
2021-01-04 10:47:08 -0600

    new

--- cars.py
+++ cars.py
@@ -7,6 +7,12 @@ class Car:
         print("New Car!")
         self.name = name
 
+    def __str__(self):
+        return f"My car the {self.name} currently {self.runs}"
+
+    def __repr__(self):
+        return f"Car({self.name})"
+
 
     def start(self):
         if self.runs:
